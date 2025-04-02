e5e347574c552d7d070a97b4a03690cd9b9e462d
Qamar Aziz <azizqamar7@gmail.com>
2025-04-02 15:15:37 +0530

    remove animation from section why home page

--- src/home/section-why.js
+++ src/home/section-why.js
@@ -3,32 +3,32 @@ import gsap from 'gsap'
 export const sectionWhy = () => {
   const vw = (coef) => window.innerWidth * (coef / 100)
 
-  const titleScrubTl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '[marquee-stack]',
-      // markers: true,
-      start: 'top 50%',
-      end: 'bottom 10%',
-      scrub: 1,
-    },
-  })
+  // const titleScrubTl = gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: '[marquee-stack]',
+  //     // markers: true,
+  //     start: 'top 50%',
+  //     end: 'bottom 10%',
+  //     scrub: 1,
+  //   },
+  // })
 
-  titleScrubTl.to('[why-title-track]', { x: '-130vw' })
+  // titleScrubTl.to('[why-title-track]', { x: '-130vw' })
 
   if (window.innerWidth > 767) {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '[why-list]',
-        // markers: true,
-        start: 'top 70%',
-        scrub: true,
-      },
-    })
+    // const tl = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '[why-list]',
+    //     // markers: true,
+    //     start: 'top 70%',
+    //     scrub: true,
+    //   },
+    // })
 
-    tl.to('[why-card="1"]', { y: '40%' })
-      .to('[why-card="2"]', { y: '-40%' }, '<')
-      .to('[why-card="3"]', { y: '40%' })
-      .to('[why-card="4"]', { y: '-40%' }, '<')
+    // tl.to('[why-card="1"]', { y: '40%' })
+    //   .to('[why-card="2"]', { y: '-40%' }, '<')
+    //   .to('[why-card="3"]', { y: '40%' })
+    //   .to('[why-card="4"]', { y: '-40%' }, '<')
 
     const carTl1 = gsap.timeline({
       scrollTrigger: {
